37b9a40bc05cc665b3ce8438e11f0a7fcc1722f0
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-02-25 21:37:13 +0500

    Implement activation functions and their derivatives in f_utils.py

--- f_utils.py
+++ f_utils.py
@@ -2,28 +2,28 @@ import numpy as np
 
 
 def tanh(a):
-    ## add code here  
+    return (np.exp(a) - np.exp(-a))/(np.exp(a) + np.exp(-a))  
 
 def tanh_derivative(a):
-   ## add code here  
+    return 1 - (tanh(a) ** 2) 
 
 def relu(a):
-    ## add code here  
+    return a if a > 0 else 0
   
 def sigmoid(a):
     return 1/(1 + np.exp(-a))
 
 def relu_derivative(a): 
-    ## add code here  
+    return 1 if a > 0 else 0
 
 def sigmoid_derivative(a):
     return np.exp(-a) / ((1 + np.exp(-a)) ** 2)
 
 def lrelu(a, k):
-    ## add code here  
+    return a if a > 0 else k * a
 
 def lrelu_derivative(a, k):  
-    ## add code here  
+    return 1 if a > 0 else k
 
 
 
